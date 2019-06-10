7bbada16caf82f59ddb3f2978e7307cedf52d59c
swissChili <swisschili@fastmail.com>
2019-06-10 12:13:52 -0700

    Added arg parser

--- QPC.Launcher/Launcher.cs
+++ QPC.Launcher/Launcher.cs
@@ -5,6 +5,61 @@
 // Date Of Creation: 5/1/2019 21:40:30
 // ============================================//
 using System;
+using System.Collections.Generic;
+
+namespace QPC.Arg {
+    enum Type {
+        None,
+        Flag,
+    }
+
+    class Parser {
+        protected string[] args;
+        public Parser(string[] args) {
+            this.args = args;
+        }
+
+        public List<string> ParseString(string lname, char sname) {
+            Type t = Type.None;
+            string last = null;
+            var values = new List<string>();
+
+            foreach (string a in args) {
+                switch(t) {
+                    case Type.None:
+                        if (a.Length > 2 && a.StartsWith("--")) {
+                            last = a.Substring(2);
+                            t = Type.Flag;
+                        } else if (a.Length > 1 && a.StartsWith("-")) {
+                            if (a.EndsWith(sname)) {
+                                last = lname;
+                                t = Type.Flag;
+                            }
+                        }
+                        break;
+
+                    case Type.Flag:
+                        if (last == lname)
+                            values.Add(a);
+                        t = Type.None;
+                        break;
+                }
+            }
+
+            return values;
+        }
+
+        public bool ParseBool(string lname, char sname) {
+            foreach (var a in args) {
+                if (a.Length > 2 && a.StartsWith("--") && a.Substring(2) == lname)
+                    return true;
+                else if (a.Length > 1 && a.StartsWith("-") && a.Contains(sname))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
 
 
 namespace QPC.Launcher
@@ -13,13 +68,7 @@ namespace QPC.Launcher
     {
 		public static void Main(string[] args)
 		{
-			string next = "";
-			if(args.Length > 1)	
-				next = args[1];
-			for(int i = 0; i < args.Length; i++, next = args[i+1])
-			{
-				
-			}
+			var p = new Arg.Parser(args);
 		}
 
 		public static void DisplayHelp()
